7fdf512a673b91ea2f9a73d3a7df634422857633
Hernany Motoso <contatohernany@gmail.com>
2022-09-24 19:08:00 -0300

    chore: add and config jest

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+};
+
+export default config;
