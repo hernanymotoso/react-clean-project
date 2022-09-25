9958ac70c916fe14dfae7a911f650de0a9d32ab0
Hernany Motoso <contatohernany@gmail.com>
2022-09-25 00:10:46 -0300

    feat: ensure `RemoteAuthentication` throws `UnexpectedError` on default cases

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,6 +1,7 @@
 import { HttpPostClient } from '@/data/protocols/http/http-post-client';
 import { HttpStatusCode } from '@/data/protocols/http/http-response';
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error';
+import { UnexpectedError } from '@/domain/errors/unexpected-error';
 import { AuthenticationParams } from '@/domain/usecases/authentication';
 
 export class RemoteAuthentication {
@@ -15,11 +16,13 @@ export class RemoteAuthentication {
       body: params,
     });
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        break;
       case HttpStatusCode.unathorized:
         throw new InvalidCredentialsError();
 
       default:
-        return Promise.resolve();
+        throw new UnexpectedError();
     }
   }
 }
